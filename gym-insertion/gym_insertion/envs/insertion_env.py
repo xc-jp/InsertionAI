db9064f7c1d548d43b82fc92ee02be017a8a372d
hoel-bagard <hoel.bagard@gmail.com>
2020-03-17 20:05:36 +0900

    Added the communication protocol for the case where the goal is stationary

--- gym-insertion/gym_insertion/envs/insertion_env.py
+++ gym-insertion/gym_insertion/envs/insertion_env.py
@@ -2,7 +2,11 @@ import socket
 import json
 import traceback
 import logging
+import base64
+import io
 
+from PIL import Image
+import numpy as np
 import gym
 # from gym import error, spaces, utils
 # from gym.utils import seeding
@@ -10,13 +14,47 @@ import gym
 
 logging.basicConfig(filename='errors.log', level=logging.WARNING)
 
-CLOSE_MSG = (json.dumps({
-    "action": "CLOSE",
-    "coord": None
-}) + "<EOF>").encode("utf-8")
+# AI_MSG =  {
+#     "action": "FIRST",  # String, can be "FIRST", "RESET", "STEP" or (optional) "CLOSE"
+#     "coord": [0, 0, 0, 0, 0, 0]  # New coord: x, y, z, alpha, beta, gamma   (3D coord)
+# }
+
+# --> FIRST means that this is the first/init message from the AI, "coord" is empty.
+#     * Response should be of type "FIRST".
+#       - "coord" shoud be the current position and orientation of the goal.
+#       - "image" shoud be the goal image, i.e., image of the connector succesfully inserted.
+#       - "done" should be None
+# --> RESET means that the AI succesfully inserted the object previously or that it took too long / went too far, "coord" is empty
+#     * Response should be of type "RESET".
+#       - "coord" should be the current position and orientation of the effector: x, y, z, alpha, beta, gamma   (3D coord)
+#       - "image" should be a picture taken from a fix point that shows both end effector and the goal.
+#       - "done" should be None
+# --> STEP means that the AI wants to move to the point in "coord"
+#     * Response should be of type "STEP".
+#       - "coord" should be the current position and orientation of the effector: x, y, z, alpha, beta, gamma   (3D coord)
+#       - "image" should be a picture taken from a fix point that shows both end effector and the goal.
+#       - "done" should be either 1 or 0. 1 means that the last action resulted in a succesful insertion, should be 0 otherwise.
+# --> CLOSE means that the AI wants to disconnect
+
+# SIM_MSG = {
+#     "action": "FIRST",  # String, can be "FIRST", "RESET" or "STEP"
+#     "image": (INT, INT),  #  Image (int)
+#     "coord": [0, 0, 0, 0, 0, 0],  # Array of 6 floats
+#     "done": 0  # Integer
+# }
 
 RESET_MSG = (json.dumps({
     "action": "RESET",
+    "coord": None
+}) + "<EOF>").encode("utf-8")
+
+FIRST_MSG = (json.dumps({
+    "action": "FIRST",
+    "coord": None
+}) + "<EOF>").encode("utf-8")
+
+CLOSE_MSG = (json.dumps({
+    "action": "CLOSE",
     "coord": None
 }) + "<EOF>").encode("utf-8")
 
@@ -50,6 +88,22 @@ def recv_end(socket, end="<EOF>", buffer_size=800000):
     return message
 
 
+def decode_message(message):
+    coord = message["coord"]
+    img = base64.b64decode(message["image"])[:-5]
+    img = Image.open(io.BytesIO(img))
+    img = np.array(img)[:, :, :3]/255  # Try to just do img/255
+    done = message["done"]
+
+    if message["action"] == "FIRST" or message["action"] == "RESET":
+        return coord, img, None
+    elif message["action"] == "STEP":
+        return coord, coord, done
+    else:
+        logging.error("Received an unexpected action: {}".format(message["action"]))
+        return -1
+
+
 class InsertionEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
@@ -59,14 +113,23 @@ class InsertionEnv(gym.Env):
         self.socket.connect((host, port))
         print("Connected to the environment")
 
-        self.action_space = 4  # Currently using a continuous action space, not applicable
-        self.observation_space_shape = (640, 640, 4,)  # Shape of the obvervation space
+        self.action_space = None  # Currently using a continuous action space, not applicable
+        self.observation_space_shape = (32, 32, 1,)  # Shape of the obvervation space
+
+    def start(self):
+        sent = self.socket.send(FIRST_MSG)
+        if sent == 0:
+            raise RuntimeError("socket connection broken")
+        message = recv_end(self.socket)
+        self.goal_img, self.goal_coord, _ = decode_message(message)
+
+        return self.goal_img, self.goal_coord
 
     def step(self, action: [float]) -> [[int], int, bool, [float]]:
         """Executes the given action
 
         Args:
-            action: action to execute, array of [???]
+            action: action to execute, array of 6 floats
 
         Returns:
             next_state: new state
@@ -75,8 +138,23 @@ class InsertionEnv(gym.Env):
                   (maybe end experiment if the robot's end goes too far off ?)
             infos: Additional infos
         """
+
+        step_msg = {}
+        step_msg["action"] = "STEP"
+        step_msg["coord"] = action
+
+        print("Step action: {}".format(step_msg))
+        step_msg = json.dumps(step_msg) + "<EOF>"
+        sent = self.socket.send(step_msg.encode("utf-8"))
+        if sent == 0:
+            raise RuntimeError("socket connection broken")
         message = recv_end(self.socket)
-        new_state, reward, done, infos = decode_message(message)
+        img, coord, done = decode_message(message)
+
+        reward = self.get_reward(img, done)
+        new_state = (img, coord)
+        infos = None
+
         return [new_state, reward, done, infos]
 
     def reset(self) -> [int]:
@@ -89,9 +167,9 @@ class InsertionEnv(gym.Env):
         sent = self.socket.send(RESET_MSG)
         if sent == 0:
             raise RuntimeError("socket connection broken")
-
         message = recv_end(self.socket)
-        new_state = decode_message(message)  # Get state from message
+        img, coord, _ = decode_message(message)
+        new_state = (img, coord)
         return new_state
 
     def render(self, mode='human', close=False):
