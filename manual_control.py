d235f357aa47a8e022d888e73de9e827b98edb73
hoel <hoel.bagard@gmail.com>
2020-03-23 15:16:33 +0900

    Small refactoring

--- manual_control.py
+++ manual_control.py
@@ -6,21 +6,12 @@ import gym_insertion
 
 def main():
     parser = argparse.ArgumentParser("Insertion, Manual mode")
-    parser.add_argument('--host', type=str, help='IP of the server')
-    parser.add_argument('--port', type=int, help='Port that should be used to connect to the server')
+    parser.add_argument('--host', default="192.168.2.121", type=str, help='IP of the server (default is a Windows server)')
+    parser.add_argument('--port', default=9090, type=int, help='Port that should be used to connect to the server')
     args = parser.parse_args()
 
-    if args.host:
-        host = args.host
-    else:
-        host = "192.168.2.121"   # Windows server
-    if args.port:
-        port = args.port
-    else:
-        port = 9090
-
     env = gym.make('insertion-v0')
-    goal_img, goal_coord = env.start(host, port)
+    goal_img, goal_coord = env.start(args.host, args.port)
     print(f"Goal coord: {goal_coord}")
 
     # Action: [0, 0, 0, 0, 0, 0]  # New coord: x, y, z, alpha, beta, gamma
